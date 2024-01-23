2151fde591ea811f333635bb0137e2a742ae3af2
Robiul72 <robiulislam366972@gmail.com>
2024-01-24 03:28:58 +0600

    add
    24-01-2024

---
+++ stripePayment/src/main/java/com/javawhizz/stripePayment/StripePaymentApplication.java
@@ -0,0 +1,15 @@
+package com.javawhizz.stripePayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePaymentApplication {
+
+    public static String apikey;
+
+    public static void main(String[] args) {
+		SpringApplication.run(StripePaymentApplication.class, args);
+	}
+
+}
