f5d2de8416a84c2225f0a38823f9fccae9c58045
Robiul72 <robiulislam366972@gmail.com>
2024-01-28 13:23:09 +0600

    add
    dfd

--- spring boot security/spring-boot-security/src/main/java/com/emranhss/spring/boot/security/model/Student.java
+++ spring boot security/spring-boot-security/src/main/java/com/emranhss/spring/boot/security/model/Student.java
@@ -1,6 +1,22 @@
 package com.emranhss.spring.boot.security.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@EqualsAndHashCode
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
 
 }
